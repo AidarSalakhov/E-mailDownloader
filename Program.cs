66e326db30996bd9c6b1a84f5c9d8f6709ce8d35
Aidar Salakhov <adr-sal@yandex.ru>
2022-07-06 11:49:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_mailDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<OpenPop.Mime.Message> messages = new List<OpenPop.Mime.Message>();
+
+                messages = EmailDownloader.FetchAllMessages("pop.yandex.ru", 995, true, "split4366", "split43664353646456456");
+
+                foreach (var item in messages)
+                {
+                    Console.WriteLine(item.MessagePart.ContentType);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+    }
+}
